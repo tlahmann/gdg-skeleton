cbf0a525a40fe50257a7e90880fca9106cb5eee2
tlahmann <tobiaslahmann@gmail.com>
2019-04-30 13:36:01 +0200

    Define third example

--- assets/a3/sketch.js
+++ assets/a3/sketch.js
@@ -1,50 +1,9 @@
-// P_2_1_1_04
-//
-// Generative Gestaltung – Creative Coding im Web
-// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
-// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
-// with contributions by Joey Lee and Niels Poldervaart
-// Copyright 2018
-//
-// http://www.generative-gestaltung.de
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * shapes in a grid, that are always facing the mouse
- *
- * MOUSE
- * position x/y        : position to face
- *
- * KEYS
- * 1-7                 : choose shapes
- * arrow up/down       : scale of shapes
- * arrow left/right    : additional rotation of shapes
- * d                   : toggle size depending on distance
- * g                   : toggle grid resolution
- * s                   : save png
- */
+// @ts-nocheck
+/// <reference path="../../p5/addons/p5.global-mode.d.ts" />
 'use strict';
 
-var tileCount = 10;
-
-var tileWidth;
-var tileHeight;
-var shapeSize = 50;
-var newShapeSize = shapeSize;
-var shapeAngle = 0;
-var maxDist;
-var currentShape;
-var shapes;
-
-var sizeMode = 0;
+let currentShape;
+let shapes;
 
 function preload () {
   shapes = [];
@@ -56,57 +15,32 @@ function preload () {
 function setup () {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  noStroke();
   // set the current shape to the first in the array
   currentShape = shapes[0];
-  tileWidth = width / tileCount;
-  tileHeight = height / tileCount;
-  maxDist = sqrt(pow(width, 2) + pow(height, 2));
 }
 
 function draw () {
-  clear();
-
-  for (var gridY = 0; gridY < tileCount; gridY++) {
-    for (var gridX = 0; gridX < tileCount; gridX++) {
-
-      var posX = tileWidth * gridX + tileWidth / 2;
-      var posY = tileHeight * gridY + tileWidth / 2;
-
-      // calculate angle between mouse position and actual position of the shape
-      var angle = atan2(mouseY - posY, mouseX - posX) + (shapeAngle * (PI / 180));
-
-      if (sizeMode == 0) newShapeSize = shapeSize;
-      if (sizeMode == 1) newShapeSize = shapeSize * 1.5 - map(dist(mouseX, mouseY, posX, posY), 0, 500, 5, shapeSize);
-      if (sizeMode == 2) newShapeSize = map(dist(mouseX, mouseY, posX, posY), 0, 500, 5, shapeSize);
-
-      push();
-      translate(posX, posY);
-      rotate(angle);
-      noStroke();
-      image(currentShape, 0, 0, newShapeSize, newShapeSize);
-      pop();
-    }
-  }
+  // if we draw a background with alpha=50 the last few frames get drawn for a short amount of time
+  background(color(255, 50));
+  // math to calculate a circle
+  let posX = (width / 2) + cos(frameCount / 10) * 50;
+  let posY = (height / 2) + sin(frameCount / 10) * 50;
+  push();
+  translate(posX, posY);
+  // draw an image 
+  image(currentShape, 0, 0, 50, 50);
+  pop();
 }
 
 function keyReleased () {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  if (key == 'd' || key == 'D') sizeMode = (sizeMode + 1) % 3;
-  if (key == 'g' || key == 'G') {
-    tileCount += 5;
-    if (tileCount > 20) {
-      tileCount = 10;
-    }
-    tileWidth = width / tileCount;
-    tileHeight = height / tileCount;
-  }
-
   if (key == '1') currentShape = shapes[0];
   if (key == '2') currentShape = shapes[1];
   if (key == '3') currentShape = shapes[2];
-
-  if (keyCode == UP_ARROW) shapeSize += 5;
-  if (keyCode == DOWN_ARROW) shapeSize = max(shapeSize - 5, 5);
-  if (keyCode == LEFT_ARROW) shapeAngle += 5;
-  if (keyCode == RIGHT_ARROW) shapeAngle -= 5;
 }
+
+// dynamically adjust the canvas size to the window size
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
